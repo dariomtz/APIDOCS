f3d63bdfc4a13f67cf47f5f23e1bc19145b01c4c
Dario Martinez <gusdamtzr@gmail.com>
2020-04-12 12:06:58 -0500

    Endpoint view first additions
    I added two empty methods that I know will be needed and will make everything easier later on.
    I also wrote a simple createHTML method that only displays the information, without any style

--- public/EndpointView.js
+++ public/EndpointView.js
@@ -1,6 +1,73 @@
-class EndpointView{
-	constructor(endpoint){
+class EndpointView extends View{
+	constructor(controller, endpoint, parentId,  editable = false){
+		super(controller);
 
+		this.setEndpoint(endpoint);
+
+		$('#endpoints-' + parentId).append(this.createHTML());
+
+		this.populateEndpoint();
+
+		if(editable){
+			//on-click events for features that only the owner of the project should have access to must go here
+
+		}else{
+			$('.edit-endpoint').remove();
+		}
+	}
+
+	/**
+	 * When we tal about a resource we mean an object of this type:
+	 * {
+			id: push.key,
+			method: method,
+			sumary: sumary,
+			description: description,
+			uriPath: uriPath,
+			requestBody: requestBody,
+			responseBody: responseBody,
+			responseStatus: responseStatus,
+		}
+
+		Set makes all this attributes available in this object.
+		Ex:
+			this.id;
+			this.method;
+			etc.
+
+		This documentation can be deleted when this method is done.
+	 */
+	setEndpoint(endpoint){
+
+	}
+
+	/** 
+	 * makes the html match the values of the endpoint.
+	 * The values of the endpoint must be set before this method is used.
+	 * */
+	populateEndpoint(){
+			
+	}
+
+	createHTML(){
+		return '\
+		<div id="' + this.id + '">\
+			<span class="h4">Method: </span>\
+			<span id="endpoint-method' + this.id + '" class="h4"></span>\
+			<span class="h4">URI Path: </span>\
+			<span id="endpoint-uriPath-' + this.id + '" class="h4"></span>\
+			<span >Summary: </span>\
+			<span id="endpoint-summary-' + this.id + '"></span>\
+			<span >Description: </span>\
+			<span id="endpoint-description-' + this.id + '"></span>\
+			<span >Request Body: </span>\
+			<span id="endpoint-req-body-' + this.id + '"></span>\
+			<span >Response Body: </span>\
+			<span id="endpoint-res-body-' + this.id + '"></span>\
+			<span >Response Status Code: </span>\
+			<span id="endpoint-res-status-' + this.id + '"></span>\
+		</div>\
+		';
 	}
 
 	
