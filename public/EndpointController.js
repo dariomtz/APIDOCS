799fc0a3db1273044a1d0c2da450784c84e30af3
Dario Martinez <gusdamtzr@gmail.com>
2020-07-27 16:59:15 -0500

    update endpoint validations

--- public/EndpointController.js
+++ public/EndpointController.js
@@ -7,6 +7,24 @@ class EndpointController extends Controller{
 
 	updateEndpoint(method, summary, description, uriPath, requestBody, responseBody, responseStatus){
 		return new Promise(resolve => {
+			
+			let validations = {
+				method: this.validateField(method, null, 'Invalid Method', 'You must select a method.'),
+				description: this.validateField(description, '', 'Invalid description', 'The description cannot be empty'),
+				uriPath: this.validateField(uriPath, '', 'Invalid URI Path', 'The URI Path cannot be empty'),
+				responseStatus: this.validateField(responseStatus, '', 'Invalid Response Code', 'The Response Code cannot be empty'),
+				responseStatus2: this.validateField(isNaN(responseStatus), true, 'Invalid Response Code', 'The Response Code must be a number'),
+			};
+			
+			for (const key in validations) {
+				if (validations.hasOwnProperty(key)) {
+					const element = validations[key];
+					if(element instanceof Error){
+						resolve(element);
+						return;
+					}
+				}
+			}
 
 			let newEndpoint = {
 				method: method,
