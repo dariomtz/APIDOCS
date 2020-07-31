cb8b4e422bc4de25c1faf742f238b2685c92158f
Dario Martinez <gusdamtzr@gmail.com>
2020-07-31 17:43:27 -0500

    Documented Model.js

--- public/Model.js
+++ public/Model.js
@@ -1,9 +1,35 @@
+/**
+ * Model:
+ * 
+ * Model represents the shape of the data. Model objects store data retrieved from the database.
+ * This Model class is abstract, meaning that it should not be used by itself, 
+ * but rather other classes that will work as a Model should inherit from it. 
+ * 
+ */
 class Model{
+    /**
+     * Creates an instance of Model.
+     * @constructor
+     * 
+     * @param {Object} fb 
+     * The firebase reference object in which the model should be added (parent reference).
+     * @param {String} id Identifier for the object.
+     */
     constructor(fb, id = null){
         this.fb = fb;
         this.id = id;
     }
 
+    /**
+     * Validates that a value is not invalid.
+     * 
+     * @param {String} fieldValue The value of the field.
+     * @param {String} invalidValue The value that must not be in this field.
+     * @param {String} errName Name of the error.
+     * @param {String} errMessage Details of the error.
+     * 
+     * @returns {Error} An instance of error if it is invalid, null otherwise.
+     */
     validateField(fieldValue, invalidValue, errName, errMessage){
 		if(fieldValue === invalidValue){
 			return this.createError(errName, errMessage);
@@ -11,6 +37,14 @@ class Model{
 		return null;
     }
 
+    /**
+     * Creates an instance of Error with the name and message passed.
+     * 
+     * @param {String} name Name of the error.
+     * @param {String} message Details of the error.
+     * 
+     * @returns {Error} An instance of Error with the name and message passed.
+     */
     createError(name, message){
         let err = new Error();
         err.name = name;
@@ -18,6 +52,15 @@ class Model{
         return err;
     }
     
+    /**
+     * Checks if slug is a valid slug.
+     * 
+     * @param {String} slug The String to check
+     * @param {String} field The name of the field to check
+     * 
+     * @returns {Error} An instance of error if it is invalid, null otherwise.
+     */
+
     validateSlug(slug, field){
 		if(typeof slug !== "string"  || slug === ''){
             return this.createError(
