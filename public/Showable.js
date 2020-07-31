8a480feef0bdb39839b60a71d310cebc411af360
Dario Martinez <gusdamtzr@gmail.com>
2020-07-31 18:28:40 -0500

    Document Showable class

--- public/Showable.js
+++ public/Showable.js
@@ -1,18 +1,40 @@
+/**
+ * Showable:
+ * 
+ * Showable is an abstract class for objects that will be related to HTML elements.
+ * All classes that extend Showable must implement a render method that returns the HTML of the object.
+ */
 class Showable {
+    
+    /**
+     * Appends the Showable's HTML to an existing HTML object.
+     * 
+     * @param {Object} parent The HTML object to which this Showable will be appended.
+     */
     async appendTo(parent){
         $(parent).append(await this.render())
         return;
     }
 
+    /**
+     * Preppends the Showable's HTML to an existing HTML object.
+     * @param {Object} parent The HTML object to which this Showable will be prepended.
+     */
     async prependTo(parent){
         $(parent).prepend(await this.render())
         return;
     }
 
+    /**
+     * Shows this Showable's HTML.
+     */
     show(){
         $('#' + this.id).removeClass('d-none');
     }
 
+    /**
+     * Hides this Showable's HTML.
+     */
     hide(){
         $('#' + this.id).addClass('d-none');
     }
